d4f64c9642156cb66070e1b69a1263763785b527
yunnus <122170438+yunnus143@users.noreply.github.com>
2023-05-12 09:36:48 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -3,9 +3,28 @@ var mongoose = require("mongoose");
 //Complete Schema so that while ading new product it should be valid.
 // No two products can have same name in Document .
 var productSchema = mongoose.Schema({
-  
+   name: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: async function(value) {
+        const productCount = await this.model('Product').countDocuments({ name: value });
+        return productCount === 0;
+      },
+      message: 'Product Name already exists'
+    }
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
 });
 
 
-
+const Product = mongoose.model('Product', productSchema);
 module.exports = mongoose.model("User", productSchema);;
